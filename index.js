2a2eab5dd9a8089f091ec04c4b32b2f598921c9b
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2017-06-12 19:19:23 -0500

    Check for winner and make players emojis

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 'use strict';
 const prompt = require('prompt');
 
-console.log('¡Bienvenidos to Tick Taco Toe!');
+console.log('Welcome to Ticket Taco Toe!');
 
-const board = [[null, null, null], [null, null, null], [null, null, null]];
+const board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']];
 
 prompt.start();
 const makeAMove = (place, player) => {
@@ -28,42 +28,55 @@ const makeAMove = (place, player) => {
     board[2][2] = player;
   }
 }
-const isWinner = (player) => {
-  const piece = player === 1 ? 'X' : 'O';
-  let winner = false;
-  if(board[0].filter(p => p=== piece) === 3)
-    winner = true;
-  if(board[1].filter(p => p=== piece) === 3)
-    winner = true;
-  if(board[2].filter(p => p=== piece) === 3)
-    winner = true;
 
-  if(winner) {
-    console.log('¡Felicidadez! ¡Player ' + player);
+const rowWinner = (player) => (
+  board.filter(r => r[0] === player).length === 3 || board.filter(r => r[1] === player).length === 3
+  || board.filter(r => r[2] === player).length === 3
+);
+
+const colWinner = (player) => (
+  board[0].filter(p => p == player).length === 3 || board[1].filter(p => p == player).length === 3
+  || board[2].filter(p => p == player).length === 3
+);
+
+const majDiagWinner = (player) => (
+  board[0][0] === player && board[1][1] === player && board[2][2] === player
+);
+
+const minDiagWinner = (player) => (
+  board[0][2] === player && board[1][1] === player && board[2][0] === player
+);
+
+const isWinner = (player) => {
+  const piece = player === 1 ? '🎟' : '🌮';
+
+  if(rowWinner(piece) || colWinner(piece) || majDiagWinner(piece) || minDiagWinner(piece)) {
+    printBoard();
+    console.log();
+    console.log(`¡Felicidadez! 🎉 ¡Player ${player} won! ${piece} ${piece} ${piece}`);
+    return true;
   }
-  return winner;
+  return false;
 };
 
-const showBoard = () => {
+const printBoard = () => {
   console.log('Board');
-  console.log(board[0]);
-  console.log(board[1]);
-  console.log(board[2]);
+  board.forEach(b => { console.log(...b); });
 }
 
 const play1 = () => {
-  showBoard();
+  printBoard();
   prompt.get('Player 1 turn', (err, result) => {
-    makeAMove(result['Player 1 turn'], 'X');
+    makeAMove(result['Player 1 turn'], '🎟');
     if(isWinner(1)) return;
     play2();
   });
 }
 
 const play2 = () => {
-  showBoard();
+  printBoard();
   prompt.get('Player 2 turn', (err, result) => {
-    makeAMove(result['Player 2 turn'], 'O');
+    makeAMove(result['Player 2 turn'], '🌮');
     if(isWinner(2)) return;
     play1();
   });
